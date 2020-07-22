b9d6c8f9254e970a8deafb84f6005fcec5d5b9d8
royeradames <royeraadames@gmail.com>
2020-07-22 11:43:48 -0400

    added styles so it does not touch the border of the windows

--- team-builder/src/components/TeamMates.js
+++ team-builder/src/components/TeamMates.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
-
+//css style
+const Styled = styled.div`
+    /* don't touch the window and center the content*/
+    margin: 2rem auto;
+    width: 80vw;
+`
 export default function TeamMates({ key, details }) {
     if (!details) {
         return <h3>Workingg fetching your TeamMates details</h3>
     }
 
     return (
-        <div className='team-mates-container' key={key}>
+        <Styled className='team-mates-container' key={key}>
             <h2>{details.username}</h2>
             <p>Email: {details.email}</p>
             <p>Role: {details.role}</p>
-        </div>
+        </Styled>
     )
 }
