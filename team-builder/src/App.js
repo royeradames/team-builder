69a67806106556b462b22a150be8c4b0bf77156d
royeradames <royeraadames@gmail.com>
2020-07-21 20:25:56 -0400

    added team form inputs, and some styles so it does not touch the sides of the window

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,25 @@
+//libraries
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
+
+//component
+import TeamMemberForm from './components/TeamMemberForm'
+
+//css style
+const Styled = styled.div`
+  .form{
+    margin: 2rem auto;
+    width: 80vw;
+  }
+`
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Styled className="form-container">
+      <TeamMemberForm/>
+    </Styled>
   );
 }
 
---
+++ team-builder/src/components/TeamMemberForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Radio,
+  Select,
+  TextArea,
+} from 'semantic-ui-react'
+
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'o', text: 'Other', value: 'other' },
+]
+
+class TeamMemberForm extends Component {
+  state = {}
+
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render() {
+    const { value } = this.state
+    return (
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field
+            control={Input}
+            label='First name'
+            placeholder='First name'
+          />
+          <Form.Field
+            control={Input}
+            label='Last name'
+            placeholder='Last name'
+          />
+          <Form.Field
+            control={Select}
+            label='Gender'
+            options={options}
+            placeholder='Gender'
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <label>Role</label>
+          <Form.Field
+            control={Radio}
+            label='React 1'
+            value='react 1'
+            checked={value === 'react 1'}
+            onChange={this.handleChange}
+          />
+          <Form.Field
+            control={Radio}
+            label='React 2'
+            value='react 2'
+            checked={value === 'react 2'}
+            onChange={this.handleChange}
+          />
+          <Form.Field
+            control={Radio}
+            label='Back end'
+            value='back end'
+            checked={value === 'back end'}
+            onChange={this.handleChange}
+          />
+          <Form.Field
+            control={Radio}
+            label='HTML and CSS front end'
+            value='HTML and CSS front end'
+            checked={value === 'HTML and CSS front end'}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Field
+          control={TextArea}
+          label='About'
+          placeholder='Tell us more about you...'
+        />
+        <Form.Field
+          control={Checkbox}
+          label='I agree to the Terms and Conditions'
+        />
+        <Form.Field control={Button}>Submit</Form.Field>
+      </Form>
+    )
+  }
+}
+
+export default TeamMemberForm
