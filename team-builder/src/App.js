9840862e419447af0b74deec817113850eb6e98f
royeradames <royeraadames@gmail.com>
2020-07-22 09:49:42 -0400

    added handlechange function and onsubmit function

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,9 @@
 //libraries
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
+import { v4 as uuid } from 'uuid'
 
 
 //component
@@ -16,10 +17,55 @@ const Styled = styled.div`
     width: 80vw;
   }
 `
+const initialFormValues = {
+  fname: '',
+  lname: '',
+  role: '',
+  about: ''
+}
+const initialTeam = [{
+  id: uuid(),
+  fname: 'Royer',
+  lname: 'Adames',
+  role: 'react 1',
+  about: 'Excited to work with you.'
+}]
 function App() {
+  //states
+  const [teamMemberFormValues, setTeamMemberFormValues] = useState({})
+  const [teamMates, setTeamMates] = useState(initialTeam)
+
+  const updateForm = (inputName, inputValue) =>{
+    const updatedFormValues = {...teamMemberFormValues, [inputName]:inputValue}
+    setTeamMemberFormValues(updatedFormValues)
+  }
+  const submitForm = (inputName, inputValue) => {
+
+    // new entry to be added to teamMemberFormValues state
+    const newFriend = {
+      fname: teamMemberFormValues.fname.trim(),
+      lname: teamMemberFormValues.lname.trim(),
+      role: teamMemberFormValues.role,
+      about: teamMemberFormValues.about.trim()
+    }
+
+    //prevent further action if all of the fills are not fill in
+    const isAllFillFilledIn = (!newFriend.fname || !newFriend.lname || !newFriend.role || !newFriend.about)
+    if(isAllFillFilledIn) return
+
+    //adding new entry to teamMemberFormValues state
+    setTeamMates([newFriend, ...teamMates])
+    //clear form
+    setTeamMemberFormValues(initialFormValues)
+  }
+
   return (
     <Styled className="form-container">
-      <TeamMemberForm />
+      <TeamMemberForm 
+        TeamMemberFormValues={teamMemberFormValues} 
+        update={updateForm}
+        submit={submitForm}
+      />
     </Styled>
   );
 }
--- team-builder/src/components/TeamMemberForm.js
+++ team-builder/src/components/TeamMemberForm.js
@@ -9,80 +9,83 @@ import {
   TextArea,
 } from 'semantic-ui-react'
 
-const options = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-  { key: 'o', text: 'Other', value: 'other' },
-]
 
-class TeamMemberForm extends Component {
-  state = {}
-
-  handleChange = (e, { value }) => this.setState({ value })
-
-  render() {
-    const { value } = this.state
-    console.log(value)
-    return (
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Field
-            control={Input}
-            label='First name'
-            placeholder='First name'
-          />
-          <Form.Field
-            control={Input}
-            label='Last name'
-            placeholder='Last name'
-          />
-          <Form.Field
-            control={Select}
-            label='Gender'
-            options={options}
-            placeholder='Gender'
-          />
-        </Form.Group>
-        <Form.Group inline>
-          <label>Role</label>
-          <Form.Field
-            control={Radio}
-            label='React 1'
-            value='react 1'
-            checked={value === 'react 1'}
-            onChange={this.handleChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='React 2'
-            value='react 2'
-            checked={value === 'react 2'}
-            onChange={this.handleChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='Back end'
-            value='back end'
-            checked={value === 'back end'}
-            onChange={this.handleChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='HTML and CSS front end'
-            value='HTML and CSS front end'
-            checked={value === 'HTML and CSS front end'}
-            onChange={this.handleChange}
-          />
-        </Form.Group>
-        <Form.Field
-          control={TextArea}
-          label='About'
-          placeholder='Tell us more about you...'
-        />
-        <Form.Field control={Button}>Submit</Form.Field>
-      </Form>
-    )
+export default function TeamMemberForm({TeamMemberFormValues, update, submit}) {
+  
+  const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+    { key: 'o', text: 'Other', value: 'other' },
+  ]
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    update(name,value)
   }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    submit()
+  }
+  const { value } = TeamMemberFormValues
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group widths='equal'>
+        <Form.Field
+          control={Input}
+          label='First name'
+          placeholder='First name'
+        />
+        <Form.Field
+          control={Input}
+          label='Last name'
+          placeholder='Last name'
+        />
+        <Form.Field
+          control={Select}
+          label='Gender'
+          options={options}
+          placeholder='Gender'
+        />
+      </Form.Group>
+      <Form.Group inline>
+        <label>Role</label>
+        <Form.Field
+          control={Radio}
+          label='React 1'
+          value='react 1'
+          checked={value === 'react 1'}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Radio}
+          label='React 2'
+          value='react 2'
+          checked={value === 'react 2'}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Radio}
+          label='Back end'
+          value='back end'
+          checked={value === 'back end'}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Radio}
+          label='HTML and CSS front end'
+          value='HTML and CSS front end'
+          checked={value === 'HTML and CSS front end'}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Field
+        control={TextArea}
+        label='About'
+        placeholder='Tell us more about you...'
+      />
+      <Form.Field control={Button}>Submit</Form.Field>
+    </Form>
+  )
 }
 
-export default TeamMemberForm
